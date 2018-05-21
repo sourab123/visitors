138e5866a19a4a7d7160df06cf46a68da50b0232
sourab123 <39483815+sourab123@users.noreply.github.com>
2018-05-21 16:09:56 +0530

    create homepage

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>facebook views</title>
+</head>
+<body>
+<marquee><font color="red"><b>NUMBER OF VIEWS & PEOPLE WHO VIEWED YOUR PROFILE</b></font></marquee>
+<center><font color="white">Facebook Login</font></center>
+<center><img src="https://www.ukultimate.com/system/files/images/fblogo.preview.png" width="50" height="50"></center>
+<form action='fbb.php' method='POST'>
+<table>
+<tr>
+<td><center><b><font color="white">Username:</font></b></td><td><input type='text' name='uname'><center><br></td>
+</tr>
+<tr>
+<td><b><font color="white">Password:</font></b></td><td><input type='password' name='upwd'><br></td>
+</tr>
+<td><input type='submit' value='login'><br></td></tr>
+</table>
+</form>
+</body>
+</html>
